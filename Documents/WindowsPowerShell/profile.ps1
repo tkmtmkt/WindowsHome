c9bc15cfe2fa599921a02a946f28a5fc8e55c71f
Takamatsu Makoto <tkmtmkt@gmail.com>
2013-08-20 22:05:44 +0900

    sbt起動時のエンコーディング指定削除

--- Documents/WindowsPowerShell/profile.ps1
+++ Documents/WindowsPowerShell/profile.ps1
@@ -665,8 +665,6 @@ Function sbt {
         $argList  = @("$Env:JAVA_OPTS -Xms512M -Xmx1024M -Xss1M")
         $argList += @("-XX:MaxPermSize=200M -XX:ReservedCodeCacheSize=60M")
         $argList += @("-XX:+CMSClassUnloadingEnabled -XX:-UseGCOverheadLimit")
-        $argList += @("-Duser.language=en")
-        $argList += @("-Dfile.encoding=UTF-8")
         $argList += @("-Dsbt.global.base=`"$APPSDIR\sbt`"")
         $argList += @("-Dsbt.ivy.home=`"$APPSDIR\sbt\repository`"")
         $argList += @("-jar $sbtLaunch")
