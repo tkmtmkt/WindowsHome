c4d153a8356643e3ffb38d79be7a1049d2eec576
Takamatsu Makoto <tkmtmkt@gmail.com>
2014-05-16 07:01:01 +0900

    vimプラグイン更新用スクリプト

---
+++ .vim/bundle/update.ps1
@@ -0,0 +1,16 @@
+<#
+.SYNOPSIS
+#>
+$ps1_file = $MyInvocation.MyCommand.Path
+$base_dir = split-path $ps1_file
+$log_file = $ps1_file.replace(".ps1",".log")
+
+ls $base_dir .git -r | %{
+    $repo_dir = split-path $_.fullname
+    write-host "### $(split-path $repo_dir -leaf)"
+    pushd $repo_dir
+    git co master
+    git pull
+    popd
+}
+# vim: set ts=4 sw=4 et :
