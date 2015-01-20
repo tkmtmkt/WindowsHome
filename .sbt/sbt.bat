4b95a9579c060fdb7f323ff6763305214370424a
Takamatsu Makoto <tkmtmkt@gmail.com>
2015-01-21 06:52:00 +0900

    sbt起動スクリプトの引数をjava8用に変更

--- .sbt/sbt.bat
+++ .sbt/sbt.bat
@@ -2,7 +2,7 @@
 set SCRIPT_DIR=%~dp0
 
 set SBT_OPTS=-Xms512M -Xmx1024M -Xss1M
-set SBT_OPTS=%SBT_OPTS% -XX:MaxPermSize=200M -XX:ReservedCodeCacheSize=60M
+set SBT_OPTS=%SBT_OPTS% -XX:MaxMetaspaceSize=256M -XX:ReservedCodeCacheSize=128M
 set SBT_OPTS=%SBT_OPTS% -XX:+CMSClassUnloadingEnabled -XX:-UseGCOverheadLimit
 set SBT_OPTS=%SBT_OPTS% -Dsbt.ivy.home="%SCRIPT_DIR%repository"
 
