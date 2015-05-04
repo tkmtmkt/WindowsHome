4416d7bca26f25916743eaa51be804c15457121e
Takamatsu Makoto <tkmtmkt@gmail.com>
2015-05-04 10:00:53 +0900

    sbt.batをインストール手順を参考にして少し修正した。
    http://www.scala-sbt.org/release/tutorial/ja/Manual-Installation.html

--- .sbt/sbt.bat
+++ .sbt/sbt.bat
@@ -1,10 +1,10 @@
 @echo off
 set SCRIPT_DIR=%~dp0
 
-set SBT_OPTS=-Xms512M -Xmx1024M -Xss1M
+set SBT_OPTS=-Xms512M -Xmx1536M -Xss1M
 set SBT_OPTS=%SBT_OPTS% -XX:MaxMetaspaceSize=256M -XX:ReservedCodeCacheSize=128M
 set SBT_OPTS=%SBT_OPTS% -XX:+CMSClassUnloadingEnabled -XX:-UseGCOverheadLimit
-set SBT_OPTS=%SBT_OPTS% -Dsbt.ivy.home="%SCRIPT_DIR%repository"
+set SBT_OPTS=%SBT_OPTS%  -Dinput.encoding=Cp1252 -Dsbt.ivy.home="%SCRIPT_DIR%repository"
 
 set SBT_JAR="%SCRIPT_DIR%sbt-launch.jar"
 
