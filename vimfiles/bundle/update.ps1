c6a124036f04a1773831b81cf1f1ab3485004e1e
Takamatsu Makoto <tkmtmkt@gmail.com>
2016-11-09 00:25:28 +0900

    modify: [vim] update plugins

--- vimfiles/bundle/update.ps1
+++ vimfiles/bundle/update.ps1
@@ -5,7 +5,7 @@ $ps1_file = $MyInvocation.MyCommand.Path
 $base_dir = split-path $ps1_file
 $log_file = $ps1_file.replace(".ps1",".log")
 
-ls $base_dir .git -r | %{
+ls $base_dir .git -r -fo | %{
     $repo_dir = split-path $_.fullname
     write-host "### $(split-path $repo_dir -leaf)"
     pushd $repo_dir
